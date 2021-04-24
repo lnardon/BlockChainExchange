9d332f25e392cc49b426dea4cc55010b1204f1b0
lnardon <lucaslnardon@gmail.com>
2021-04-24 07:54:46 -0300

    ✨ Add buy token function

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.16;
 
+import './NRDToken.sol';
+
 contract EthSwap {
     string public name = "NRD BlockChain Exchange";
+    NRDToken public token;
+    uint public rate = 100;
+
+    constructor(NRDToken _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+    }
 }
