acf7484ad6244fccc7524649da838b883967d21d
lnardon <lucaslnardon@gmail.com>
2021-04-24 04:33:20 -0300

    🚀 Update smart contracts deployer

--- migrations/2_DeployContracts.js
+++ migrations/2_DeployContracts.js
@@ -1,5 +1,15 @@
 const EthSwap = artifacts.require("EthSwap");
+const NRDToken = artifacts.require("NRDToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async deployer => {
+  // Deploys Exchange Swap Smart Contract
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  // Deploys Exchange Swap Smart Contract
+  await deployer.deploy(NRDToken);
+  const nrdToken = await NRDToken.deployed();
+
+  // Transfer all tokens to Smart Contract
+  await nrdToken.transfer(ethSwap.address, "1000000000000000000000000");
 };
