0995ee65dfb4d63fe085c3f2862c9e2d8c4e025c
lnardon <lucaslnardon@gmail.com>
2021-04-27 00:58:42 -0300

    ✨ Add action button

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,29 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [tab, setTab] = useState(false);
+  const [tab, setTab] = useState("BUY");
 
   return (
     <div className="container">
       <h1 className="title">NRD BlockChain Exchange</h1>
       <div className="tabsContainer">
-        <button className="tabButton" onClick={() => setTab(!tab)}>
+        <button className="tabButton" onClick={() => setTab("BUY")}>
           Buy NRDTokens
         </button>
-        <button className="tabButton" onClick={() => setTab(!tab)}>
+        <button className="tabButton" onClick={() => setTab("SELL")}>
           Sell NRDTokens
         </button>
       </div>
+      {tab === "BUY" && <div> </div>}
+      {tab === "BUY" ? (
+        <button className="actionBtn" onClick={() => alert("NRD")}>
+          Buy tokens
+        </button>
+      ) : (
+        <button className="actionBtn" onClick={() => alert("NRD")}>
+          Sell tokens
+        </button>
+      )}
     </div>
   );
 };
