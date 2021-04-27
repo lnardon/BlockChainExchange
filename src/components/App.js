6ac5e1285cd2c3e1272972d70962041c707551d9
lnardon <lucaslnardon@gmail.com>
2021-04-26 22:41:41 -0300

    ✨ Add basic tabs structure and style

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [tab, setTab] = useState(false);
+
   return (
-    <div>
-      <h1>NRD</h1>
+    <div className="container">
+      <h1 className="title">NRD BlockChain Exchange</h1>
+      <div className="tabsContainer">
+        <button className="tabButton" onClick={() => setTab(!tab)}>
+          Buy NRDTokens
+        </button>
+        <button className="tabButton" onClick={() => setTab(!tab)}>
+          Sell NRDTokens
+        </button>
+      </div>
     </div>
   );
 };
