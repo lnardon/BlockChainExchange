56b54b906e1666fae1f08fa79e42a479383976d4
Lucas Nardon <lucaslnardon@gmail.com>
2022-07-19 00:12:34 -0300

    ✨ Add SellTokens Basic component structure

---
+++ src/components/Modal/content/SellToken/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./styles.css";
+
+interface ISellToken {
+  sellTokens: (amount: number) => {};
+}
+
+const SellToken = ({ sellTokens }: ISellToken) => {
+  const [amount, setAmount] = useState(1);
+
+  return (
+    <div className="sellTokenContainer">
+      <h1 className="sellTokenTitle">Sell NRDT</h1>
+      <input
+        className="sellTokenInput"
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+      />
+      <button className="buyBtn" onClick={() => sellTokens(amount)}>
+        Sell 1 NRDT
+      </button>
+    </div>
+  );
+};
+
+export default SellToken;
