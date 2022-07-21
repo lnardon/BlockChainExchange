2030744d391178f1458db65c473b3f4c0206813f
Lucas Nardon <lucaslnardon@gmail.com>
2022-07-21 04:31:40 -0300

    ✨ Add eth and nrdt balance

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Web3 from "web3";
 import { AbiItem } from "web3-utils";
+import NRDToken from "./contracts/NRDToken.json";
 import NRDTDEX from "./contracts/NRDTDEX.json";
 import "./App.css";
 
@@ -17,14 +18,32 @@ declare global {
 
 function App() {
   const [web3, setWeb3] = useState(new Web3());
-  const contractAddress = "0x7A614d0A3d4de47c884225515274beda0C48c4b9";
+  const contractAddress = "0xf78B6cC281A3856de0CDbFe3Ddfe992CC8ec6a73";
+  const nrdtAddress = "0xc4afF16D64e2508fc7338708BB31466154947596";
   const contractInteraction = new web3.eth.Contract(
     NRDTDEX.abi as AbiItem[],
     contractAddress
   );
+  const tokenInteraction = new web3.eth.Contract(
+    NRDToken.abi as AbiItem[],
+    nrdtAddress
+  );
   const [account, setAccount] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const [ethBalance, setEthBalance] = useState("");
+  const [nrdtBalance, setNrdtBalance] = useState("");
   const [modalContentIndex, setModalContentIndex] = useState(7);
+
+  useEffect(() => {
+    if (account) {
+      (async () => {
+        const response = await tokenInteraction.methods
+          .balanceOf(account)
+          .call();
+        setNrdtBalance(response);
+      })();
+    }
+  }, [account]);
 
   async function activate() {
     if (window.ethereum) {
@@ -42,6 +61,8 @@ function App() {
     setWeb3(web3);
     const accounts = await web3.eth.getAccounts();
     setAccount(accounts[0]);
+    const balance = await web3.eth.getBalance(accounts[0]);
+    setEthBalance(web3.utils.fromWei(balance));
   }
 
   function isLoggedIn() {
@@ -63,8 +84,8 @@ function App() {
         setModalContentIndex(3);
         setIsOpen(true);
         let response = await contractInteraction.methods
-          .buyTokens()
-          .send({ from: account, value: amount });
+          .buyTokens(amount)
+          .send({ from: account, value: amount * 10000000000000000 });
         alert(response);
         console.log(response);
       } catch (err: any) {
@@ -83,8 +104,8 @@ function App() {
         setModalContentIndex(3);
         setIsOpen(true);
         let response = await contractInteraction.methods
-          .sellTokens()
-          .send({ from: account, value: amount });
+          .sellTokens(amount)
+          .send({ from: account, value: 0 });
         alert(response);
         console.log(response);
       } catch (err: any) {
@@ -117,6 +138,14 @@ function App() {
         <button className="connectWalletBtn" onClick={activate}>
           Connect Wallet
         </button>
+      )}
+      {ethBalance && (
+        <h3 style={{ color: "white" }}>
+          Ethereum Balance: {ethBalance.slice(0, 7)}
+        </h3>
+      )}
+      {nrdtBalance && (
+        <h3 style={{ color: "white" }}>NRDT Balance: {nrdtBalance}</h3>
       )}
       {account ? (
         <div>
